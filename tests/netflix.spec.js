6973ea3485c8d11175d01bffadc25778e22e515e
Sabas Segovia <sasegovia@ucdavis.edu>
2022-07-25 20:13:53 -0700

    feat: testing complete

--- tests/netflix.spec.js
+++ tests/netflix.spec.js
@@ -1,63 +1,56 @@
 const { test, expect } = require("@playwright/test");
-
 // run tests in headful mode so you can see the browser
-test.use({ headless: false, slowMo: 1000 });
+test.use({ headless: false, slowMo: 10000 });
 
-// test("my first test", async ({ page }) => {
-//   // go to Netflix.com
-//   await page.goto("https://www.netflix.com");
 
-//   // assert page title appears
-//   await expect(page.locator('[data-uia="hero-title"]')).toHaveText(
-//     "Unlimited movies, TV shows, and more."
-//   );
-// });
 
 test("attempting a failed log-in for netflix", async ({ page }) => {
+
   // navigate to netflix.com
   await page.goto('https://www.netflix.com/');
+  await page.screenshot({ path: 'screenshots/pageOpen.png' });
 
   // Click a:has-text("Sign In")
   await page.locator('a:has-text("Sign In")').click();
   await expect(page).toHaveURL('https://www.netflix.com/login');
+  await expect(page.locator('[data-uia=login-page-title]')).toHaveText("Sign In");
+  await page.screenshot({ path: 'screenshots/emptyInputBoxes.png' });
 
   // Click text=Email or phone number
+  // Check the email/phone number input box is empty
   await page.locator('text=Email or phone number').click();
+  await expect(page.locator('input[name="userLoginId"]')).toBeEmpty();
 
   // Fill input[name="userLoginId"]
+  // Confirming automated text entered correctly
   await page.locator('input[name="userLoginId"]').fill('wrongEmail@dontLogMeIn.com');
+  await expect(page.locator('input[name="userLoginId"]')).toHaveValue('wrongEmail@dontLogMeIn.com');
 
   // Click input[name="password"]
+  // Confirming password input form is empty
   await page.locator('input[name="password"]').click();
+  await expect(page.locator('input[name="password"]')).toBeEmpty();
 
   // Fill input[name="password"]
+  // Confirming automated text entered correctly
   await page.locator('input[name="password"]').fill('WrongPasswordDoNotLogMeIn');
+  await expect(page.locator('input[name="password"]')).toHaveValue('WrongPasswordDoNotLogMeIn');
+  await page.screenshot({path: 'screenshots/passwordInput.png'})
+
 
   // Click button:has-text("Sign In")
+  // Want to be sure we are on desired page before clicking "Sign In"
   await Promise.all([
     page.waitForNavigation({ url: 'https://www.netflix.com/login' }),
     page.locator('button:has-text("Sign In")').click()
   ]);
 
+  // Confirms the failed log in attempt. Prompts user to create account.
+  await expect(page.locator('[data-uia=text]')).toHaveText("Sorry, we can't find an account with this email address. Please try again or create a new account.")
+  await page.screenshot({ path: 'screenshots/failedLoginMessage.png' });
+
+  // take user to create new account once incorrect information has been submitted
+  await page.locator('text=create a new account').click();
+  await expect(page).toHaveURL('https://www.netflix.com');
+
 });
-
-// ADD YOUR TESTS HERE!
-//sign in button
-{/* <a href="/login" class="authLinks redButton" data-uia="header-login-link">Sign In</a> */}
-
-// email input
-{/* <input type="text" data-uia="login-field" name="userLoginId" class="nfTextField error" id="id_userLoginId" value="" tabindex="0" autocomplete="email" dir="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGP6zwAAAgcBApocMXEAAAAASUVORK5CYII=&quot;); cursor: auto;"></input> */}
-
-
-//passwowd
-{/* <input type="password" data-uia="password-field" name="password" class="nfTextField error" id="id_password" value="" tabindex="0" autocomplete="password" dir="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGP6zwAAAgcBApocMXEAAAAASUVORK5CYII=&quot;); cursor: auto;"></input> */ }
-
-//submit button
-{/* <button class="btn login-button btn-submit btn-small" type="submit" autocomplete="off" tabindex="0" data-uia="login-submit-button">Sign In</button> */ }
-
-
-//invalid email
-{/* <button class="btn login-button btn-submit btn-small" type="submit" autocomplete="off" tabindex="0" data-uia="login-submit-button">Sign In</button> */ }
-
-//incorrect password
-{/* <div data-uia="error-message-container" class="ui-message-container ui-message-error"><div class="ui-message-icon"></div><div data-uia="text" class="ui-message-contents"><b>Incorrect password.</b> Please try again or you can <a href="/loginHelp">reset your password.</a></div></div><div data-uia="error-message-container" class="ui-message-container ui-message-error"><div class="ui-message-icon"></div><div data-uia="text" class="ui-message-contents"><b>Incorrect password.</b> Please try again or you can <a href="/loginHelp">reset your password.</a></div></div> */}
